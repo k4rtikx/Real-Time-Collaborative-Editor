e2b845670c75a3099c9c5aaa244981a7004e140e
kartik <kartiksingh1111116@gmail.com>
2026-06-24 20:23:23 +0530

    Fix routing: home page as landing, Redis URL fallback in views, fix createBtn redirect

--- documents/urls.py
+++ documents/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home"),
-    path("home",views.home,name="homepage"),
+    path("", views.home, name="home"),
+    path("home",views.index,name="homepage"),
     path("<str:room_id>/", views.room, name="room"),
 ]
